7392072a503ad92389e338c7743e2e58e59cafb6
chououtside <chouming3@gmail.com>
2017-05-25 22:50:23 -0400

    Update App
    Set initial state of App
    Create searchPerson method which updates the state with a person object
    Pass Props to Info and Search component
    
     Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import Search from './search';
 import Info from './info';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      infoObject: {}
+    };
+
+    this.searchPerson = this.searchPerson.bind(this);
+  }
+  
+  searchPerson (email) {
+    axios({
+      method: 'get',
+      url: `https://api.fullcontact.com/v2/person.json?email=${email}&apiKey=f6bbbb0ebc51003`,
+      responseType: 'json'
+    })
+    .then((response) => {
+      this.setState({
+        infoObject: response.data
+      })
+      console.log(this.state.infoObject);
+    })
+    .catch(function(error){
+      console.log('this is', error)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Search />
-        <Info />
+        <Search search={this.searchPerson}/>
+        <Info infoObject={this.state.infoObject}/>
       </div>
     );
   }
