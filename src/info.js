5509ccc2b8cf248505a86b0da478defe94ee7f29
chououtside <chouming3@gmail.com>
2017-05-26 02:39:18 -0400

    Update Info Component
    Render data from infoObject generated from the API
    
     Changes to be committed:
    	modified:   src/info.js

--- src/info.js
+++ src/info.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const Info = () => (
-  <div>
-    <div>Photo</div>
-    <div>Full Name</div>
-    <div>General Location</div>
-    <div>Links to all Given Profiles</div>
+const Info = ({ infoObject }) => {
+  const links = !infoObject.socialProfiles ? [] : infoObject.socialProfiles;
+
+  return <div>
+    <div><img src={!infoObject.photos ? 'https://goo.gl/n4mq73' : infoObject.photos[0].url} className="photo"/></div>
+    <div>Full Name: {!infoObject.contactInfo ? 'N/A' : infoObject.contactInfo.fullName}</div>
+    <div>General Location: {!infoObject.demographics ? 'N/A' : infoObject.demographics.locationGeneral}</div>
+    <div>
+      Links to all Given Profiles
+      <ul>
+        {links.map((profile) => <li>{profile.url}</li>)}
+      </ul>
+    </div>
   </div>
-);
+};
 
 export default Info;
